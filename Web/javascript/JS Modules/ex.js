f4fbe8355b0df7c7595567df963cfa34f5148450
MarcRGM <marcrodniemanalo@gmail.com>
2025-06-15 10:59:50 +0800

    Math module example

---
+++ Web/javascript/JS Modules/ex.js
@@ -0,0 +1,6 @@
+import {add, PI} from 'math.js';
+
+console.log(add(2, 3)); // Outputs: 5
+console.log(PI); // Outputs: 3.14159
+// The code imports the add function and PI constant from the math.js module
+
