ebb28f447b2c75c5132668567ddf9c98e055117b
MarcRGM <marcrodniemanalo@gmail.com>
2025-06-17 18:05:17 +0800

    Create sqlite database

--- Web/basicNoteApp/server.js
+++ Web/basicNoteApp/server.js
@@ -9,3 +9,11 @@ app.use(bodyParser.json()); // Lets Express read JSON data in requests.
 app.use(express.static('.')); // Serves static files from the current directory, allowing access to HTML, CSS, and JS files.
 // . means the current directory, so it serves files like index.html, style.css, and script.js.
 
+const db = new sqlite3.Database('notes.db'); // Creates or opens a SQLite database file named 'notes.db'.
+
+// uses ` to allow multi-line SQL queries
+db.run(`CREATE TABLE IF NOT EXISTS notes (
+  id INTEGER PRIMARY KEY AUTOINCREMENT, // id: Unique identifier for each note, automatically increments.
+  text TEXT NOT NULL
+)`);
+
